6f339b20fddb4ec34cf93c96c7d6716d32b315e6
pgrath <p.jmcgrath@gmail.com>
2015-10-12 18:26:09 -0400

    case 1 done

--- assign1/assign1.php
+++ assign1/assign1.php
@@ -1,5 +1,3 @@
-
-
 <style>
 
 table { border: 2px solid red; padding: 10px;}
@@ -17,17 +15,19 @@ print "Connected to MySQL<br>";
 mysql_select_db( $project );
 
 $s="select * from registered";  
+$grades="select * from grades";
 print "<br>$s<br><br>";
 
 //1. GET results of select and store them in $t
 ( $t = mysql_query ( $s  ) ) or die ( mysql_error() );
+//get data from select grades and store it in $gdat
+( $gdat = mysql_query ($grades)) or die ( mysql_error());
 
 //print "Rows are:  "  .  mysql_num_rows ($t)  . "<br>" ;
 print "<br>";
 //2. GET the successive ROWS $r  in $t
 
 $pwd = "2qaz";
-$password="123345";
 $case=4;
 $admin="admin";
 
@@ -37,14 +37,54 @@ $adminVal = $_REQUEST["adminName"];
 $usr = $_REQUEST["userName"];
 
 //Code for cases
+/*
 if($password === $pwd ) {
 	print"password is correct";
 } else{
 	die("password is incorrect");
-}
+} */
 if ($admin === $adminVal and $pwd === $password and !isset(userName)){
 	$case = 1;
-	print"The case is case 1";
+	print"The case is case 1, selecting all from grades";
+	
+	//grab the info for the table. styling set up top
+print "<table border=1>"; print"\n\r";
+print"<tr>Number of rows: ". mysql_num_rows ($gdat) . "</tr>"; print"\n\r";
+print "<tr> <td> User </td>	<td> Email </td>	<td> PWD </td>	</tr>"; print"\n\r";
+ 
+while (   $r = mysql_fetch_array($gdat) )
+{	//3. GET COLUMNS/CELLS OF ROW $r["---"]
+	$user = $r["user"];
+	$course = $r["course"];
+	$a1  = $r["A1"];
+	$a1s  = $r["A1S"];
+	$a2  = $r["A2"];
+	$a2s  = $r["A2S"];
+	$part  = $r["part"];
+	$total  = $r["total"];
+	$percent  = $r["percent"];
+	
+	//print the table
+	print"<tr>";
+	
+		print"<td>"	;	print $user	; 		print"</td>"	;	print"\n\r";
+		print"<td>"	;	print $course	; 		print"</td>"	; print"\n\r";
+		print"<td>"	;	print $a1	; 		print"</td>"	; print"\n\r";
+		print"<td>"	;	print $a1s	; 		print"</td>"	; print"\n\r";
+		print"<td>"	;	print $a2	; 		print"</td>"	; print"\n\r";
+		print"<td>"	;	print $a2s	; 		print"</td>"	; print"\n\r";
+		print"<td>"	;	print $part	; 		print"</td>"	; print"\n\r";
+		print"<td>"	;	print $total	; 		print"</td>"	; print"\n\r";
+		print"<td>"	;	print $percent	; 		print"</td>"	; print"\n\r";
+	
+	
+	print"</tr>";
+	
+}
+
+print "</table>";
+//
+	
 }
 if ($admin === $adminVal and $pwd === $password and isset(userName)){
 	$case = 2;
